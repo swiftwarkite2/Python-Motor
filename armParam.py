9f8b423e2269d5f0a5907265a1e8982a1626c3fb
Ethan Brown <88006720+swiftwarkite2@users.noreply.github.com>
2025-07-18 15:16:40 -0700

    Update armParam.py

--- armParam.py
+++ armParam.py
@@ -1,31 +1,28 @@
-# Inverted Pendulum Parameter File
+# I created this file came from the following github https://github.com/byu-controlbook/controlbook_public
 import numpy as np
-# import control as cnt
 
-# Physical parameters of the arm known to the controller
-m = 0     # Mass of the arm, kg
-ell = 0    # Length of the arm, m
+# Physical parameters known to the controller
+m = 0     # Mass kg
+ell = 0    # Length m
 g = 9.8       # Gravity, m/s**2
+# This KD value was determined from the simulation results using my calculated KT and J 
+# values. Given KT and J, this KD value is required to achieve 6600 RPM/volt specified by the 
+# hobby motor data sheet. Functionally KD is the damping effect and it provides stability
 
-KD  = 0.0000021    # /10 # value choosen so that measured RPM is 6600/v like the hobby motor
-#KD  = 0.0000021
-
+KD  = 0.0000021    # /10 # value chosen so that measured RPM is 6600/v like the hobby motor
+# J is moment of inertia (rotational motion) calculated by the following equation: 
+# Mass*Radius^2 (KG*M^2)
 J = 0.00000018772 #mass*radius^2 of wheel
-
-#b = 0.1 #n.m.s viscous friction coefficient
+# KT is the motor torque constant (KT= 60/2pi*KV; KV = 6600 rpm) multiplied by voltage
 KT = 0.00144686311 #/100 # n.m/A motor torque constant    (= 60/2pi*KV; KV = 6600 rpm) multiplied by voltage
-#KT = 0.00144686311
-#R = 1 #ohm resistence; guess
-#T = 0.0198708 #n.m external load torque        
-
 # parameters for animation
-length = 5.0    # length of arm in animation
-width = 5.0   # width of arm in animation
+length = 5.0    # length of wheel in animation
+width = 5.0   # width of wheel in animation
 
 # Initial Conditions
-theta0 = 0.0 *np.pi/180  # ,rads
-thetadot0 = 0.0         # ,rads/s
-thetaddot0 = 0.0
+theta0 = 0.0 *np.pi/180  # rads
+thetadot0 = 0.0         # rads/s
+thetaddot0 = 0.0       # rads/s^2
 
 # Simulation Parameters
 t_start = 0.0  # Start time of simulation
