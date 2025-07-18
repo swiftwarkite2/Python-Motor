95cec05a36094aa08d33e7e6a4252f20d8f1e8a5
Ethan Brown <88006720+swiftwarkite2@users.noreply.github.com>
2025-07-18 15:20:37 -0700

    Update armAnimation.py

--- armAnimation.py
+++ armAnimation.py
@@ -1,19 +1,9 @@
+# I created this file from the following github https://github.com/byu-controlbook/controlbook_public
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import numpy as np
 import armParam as P
-# if you are having difficulty with the graphics, 
-# try using one of the following backends 
-# See https://matplotlib.org/stable/users/explain/backends.html
-# import matplotlib
-# matplotlib.use('qtagg')  # requires pyqt or pyside
-# matplotlib.use('ipympl')  # requires ipympl
-# matplotlib.use('gtk3agg')  # requires pyGObject and pycairo
-# matplotlib.use('gtk4agg')  # requires pyGObject and pycairo
-# matplotlib.use('gtk3cairo')  # requires pyGObject and pycairo
-# matplotlib.use('gtk4cairo')  # requires pyGObject and pycairo
-# matplotlib.use('tkagg')  # requires TkInter
-# matplotlib.use('wxagg')  # requires wxPython
+
 
 
 class armAnimation:
@@ -35,11 +25,8 @@ class armAnimation:
     def update(self, x):
         # Process inputs to function
         theta = x[0][0]   # angle of arm, rads
-        ##X = [0, self.length*np.cos(theta)]  # X data points
-        ##Y = [0, self.length*np.sin(theta)]  # Y data points
-        # When the class is initialized, a line object will be
-        # created and added to the axes. After initialization, the
         # line object will only be updated.
+        # The following 13 lines of code are used the create the shape of the image that is shown when running my simulation
         pts =np.matrix([
             [self.width/2.0, -self.width/2.0],
             [self.width/2.0, -self.width/6.0],
@@ -53,26 +40,23 @@ class armAnimation:
             [- self.width/2.0 - self.width/6.0, -self.width/6.0],
             [- self.width/2.0, -self.width/6.0],
             [-self.width/2.0, -self.width/2.0]]).T
-        R = np.array([[np.cos(theta), np.sin(theta)],
+        R = np.array([[np.cos(theta), np.sin(theta)], # This and the following 3 lines are used to  
+                                                        # spin the object when simulated
                       [-np.sin(theta), np.cos(theta)]])
         pts = R @ pts
         xy = np.array(pts.T)
         if self.flagInit == True:
-            # Create the line object and append its handle
-            # to the handle list.
-            ##line, = self.ax.plot(X, Y, lw=5, c='blue')
-            ##self.handle.append(line)
-            ##self.flagInit=False
-                 # Create the Rectangle patch and append its handle
+
             # to the handle list
             self.handle.append(mpatches.Polygon(xy,
-                                                facecolor='blue',
-                                                edgecolor='black'))
+                                                facecolor='blue', # This line creates the blue color of my shape
+                                                edgecolor='black')) # This line creates the black edge of my shape
             # Add the patch to the axes
-            self.ax.add_patch(self.handle[0]) 
+            self.ax.add_patch(self.handle[0]) # this line makes the dashed black line to 
+                                                # represent the motor
+
             self.flagInit = False
         else:
-            ##self.handle[0].set_xdata(X)   # Update the line
-            ##self.handle[0].set_ydata(Y)
+
             self.handle[0].set_xy(xy) 
         plt.draw()
